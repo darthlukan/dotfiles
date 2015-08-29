788922ad08da6bd0c3ef2a89ad46fe42b75bb916
Brian Tomlinson <darthlukan@gmail.com>
2015-08-29 17:30:34 +0200

    Add disk usage and current datetime to bar output, cleanup bytes conversion and use a function for it

--- bin/sysinfo.py
+++ bin/sysinfo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-#
-# Author: Brian Tomlinson <darthlukan@gmail.com>
-# License: GPL2
-
 import os
 import psutil
+from datetime import datetime
+
+
+def convert_bytes(data):
+    return float(data/1024/1024/1024)
 
 
 def load():
@@ -15,20 +16,47 @@ def load():
 
 def mem():
     m = psutil.virtual_memory()
-    total = "{0:.2f}".format(float(m.total/1024/1024/1024))
+    total = "{0:.2f}".format(convert_bytes(m.total))
     used = "{0:.2f}".format(
-        float((m.total/1024/1024/1024) * (m.percent/100)))
+        convert_bytes(m.total) * (m.percent/100))
+
     return "RAM: {0}/{1}GB".format(used, total)
 
 
 def cpu():
+    psutil.cpu_percent(interval=1, percpu=False)
     percentage = "CPU: {0}%".format(
-        psutil.cpu_percent(interval=1, percpu=False))
+        psutil.cpu_percent(interval=None, percpu=False))
+
     return percentage
 
 
+def disks():
+    disks_string = "DISKS( "
+
+    for part in psutil.disk_partitions(all=False):
+        if part.device != '/dev/sda1':
+            usage = psutil.disk_usage(part.mountpoint)
+
+            if part.device == '/dev/sda2':
+                disks_string += '/: '
+            elif part.device == '/dev/sdb2':
+                disks_string += '$HOME: '
+
+            disks_string += "{0:.2f}/{1:.2f}GB ".format(
+                convert_bytes(usage.used), convert_bytes(usage.total))
+
+    disks_string += ")"
+    return disks_string
+
+
+def date_time():
+    return datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
+
 def main():
-    return "{0} | {1} | {2}".format(load(), cpu(), mem())
+    return "{0} | {1} | {2} | {3} | {4}".format(
+        disks(), cpu(), mem(), load(), date_time())
 
 
 if __name__ == '__main__':
