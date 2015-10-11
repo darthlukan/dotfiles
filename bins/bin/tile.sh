f576c5ae0da626d9865947b002935fb16b6f2a3f
Brian Tomlinson <darthlukan@gmail.com>
2015-10-11 18:28:13 +0200

    first steps toward tiling that doesn't get rekt on more than one monitor

--- bins/bin/tile.sh
+++ bins/bin/tile.sh
@@ -14,11 +14,13 @@ BW=$(wattr b $PFW)
 
 # get root window's size (beware, multi-head setups...)
 ROOT=$(lsw -r)
-SW=$(wattr w $ROOT)
+HEADS=$(xrandr | grep ' connected' | sed -e 's/\([A-Z0-9]\+\) connected.*/\1/' | wc -l)
+SW=$(($(wattr w $ROOT) / $HEADS)) # Account for multiple heads
+
 SH=$(wattr h $ROOT)
 
 # get the number of windows to put in the stacking area
-MAX=$(lsw|grep -v $PFW|wc -l)
+MAX=$(($(lsw | grep -v $PFW | wc -l) / $HEADS))  # MAX = windows per HEAD
 
 # calculate usable screen size (without borders and gaps)
 SW=$((SW - GAP - 2*BW))
@@ -29,11 +31,25 @@ Y=$((0 + GAP + PANEL))
 wtp $GAP $Y $((MASTER - GAP - 2*BW)) $((SH - GAP)) $PFW
 
 # and now, stack up all remaining windows on the right
-X=$((MASTER + GAP))
-W=$((SW - MASTER - GAP))
-H=$((SH / MAX - GAP - 2*BW))
 
-for wid in $(lsw|grep -v $PFW); do
-    wtp $X $Y $W $H $wid
-    Y=$((Y + H + GAP + 2*BW))
+LIMIT=$(($HEADS - 1))
+for ((i=0;i<=$LIMIT; i++)); do
+    if [ $i -gt 0 ]; then
+        X=$((MASTER + GAP + $((SW * i))))
+    else
+        X=$((MASTER + GAP))
+    fi
+    W=$((SW - MASTER - GAP))
+    H=$((SH / MAX - GAP - 2*BW))
+
+    WINDOW_COUNT=0
+    for wid in $(lsw | grep -v $PFW); do
+        WINDOW_COUNT=$(($WINDOW_COUNT + 1))
+        if [ $WINDOW_COUNT -lt $MAX ]; then
+            wtp $X $Y $W $H $wid
+            Y=$((Y + H + GAP + 2*BW))
+        else
+            break
+        fi
+    done
 done
