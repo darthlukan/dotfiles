e5796a9bb73e7dd8fe4d6d4a5c9e49545e66d6da
Brian Tomlinson <btomlins@redhat.com>
2022-04-21 17:41:52 -0700

    don't use -eq for string equality, use positive logic

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -17,12 +17,12 @@ HOME_PERCENT=$(df -h | grep "home" | awk '{print $5}')
 
 WIFI_IP4=$(ip -j a | jq '.[1].addr_info[0].local' | sed 's/"//g')
 WIFI_SSID=$(nmcli -m tabular -f NAME,TYPE -o c show --active | grep "wifi" | awk '{print $1}')
-VPN_ACTIVE=$(nmcli -m tabular -f TYPE -o c show --active | grep "vpn")
+VPN_ACTIVE=$(nmcli -m tabular -f TYPE -o c show --active | grep "vpn" | awk '{print $1}')
 
-if [ ! $VPN_ACTIVE -eq "vpn" ]; then
-    VPN_STATE="🔓"
-else
+if [ $VPN_ACTIVE == "vpn" ]; then
     VPN_STATE="🔒"
+else
+    VPN_STATE="🔓"
 fi
 
 echo -n "💾 $HOME_USED/$HOME_TOTAL $HOME_PERCENT | 📡 $WIFI_SSID $WIFI_IP4 $VPN_STATE | ⚡ $BAT_STATUS $BAT_PERCENTAGE | 🗓 $DATE_FMT"
